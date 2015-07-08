f19851686006e91eef274e4726e925a09071abdb
NSemakov <nsemakov@yahoo.com>
2015-07-08 23:07:00 +0400

    Revert "Revert "2""
    This reverts commit 07dd055815161fa1cb6fa197d92eb1a89a88f00c.

--- arrays/AppDelegate.m
+++ arrays/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"sdf");
-    
+    NSLog(@"opapa");
     
     return YES;
 }
